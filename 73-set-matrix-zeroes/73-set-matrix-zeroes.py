ed28fc121e50797b00a98be22f787393f39c6131
Aleksandr Piksasov <alksndr.zln@gmail.com>
2023-05-10 12:15:59 +0300

    Time: 139 ms (26.24%), Space: 17.3 MB (8.77%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -6,10 +6,9 @@ class Solution:
         to_be_zeroed = {line: [] for line in range(len(matrix))}
         for ind_line1, line1 in enumerate(matrix):
             for ind_num, num in enumerate(line1):
-                
                 if num == 0:
                     to_be_zeroed[ind_line1] = 'all'
-                    for ind_line2, line2 in enumerate(matrix):
+                    for ind_line2 in range(len(matrix)):
                         if to_be_zeroed[ind_line2] != 'all':
                             to_be_zeroed[ind_line2].append(ind_num)
                                
