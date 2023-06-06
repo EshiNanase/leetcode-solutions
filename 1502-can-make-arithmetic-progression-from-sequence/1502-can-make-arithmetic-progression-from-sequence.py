cefe865864ecd7c652e22f206a7357f78bde5d28
Aleksandr Piksasov <alksndr.zln@gmail.com>
2023-06-06 21:49:02 +0300

    Time: 64 ms (8.59%), Space: 16.5 MB (20.83%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
@@ -3,7 +3,6 @@ class Solution:
         arr.sort()
         diff = arr[1] - arr[0]
         for ind, num in enumerate(arr[:-1]):
-            print(arr[ind] - arr[ind-1])
             if arr[ind+1] - arr[ind] != diff:
                 return False
         return True
