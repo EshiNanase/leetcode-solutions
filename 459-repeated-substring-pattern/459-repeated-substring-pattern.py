7660e5cc7bdf7ea5eb07f67f2544413e144df7b1
Aleksandr Piksasov <alksndr.zln@gmail.com>
2023-06-30 07:55:09 +0300

    Time: 673 ms (7.42%), Space: 66 MB (5.03%) - LeetHub

---
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.py
@@ -0,0 +1,10 @@
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        substrings = [s[:ind] for ind, val in enumerate(s, start=1)]
+        for substring in substrings:
+            difference = len(s) // len(substring)
+            if len(s) != len(substring) and substring * difference == s:
+                print(substring, difference)
+                return True
+        return False
+        
