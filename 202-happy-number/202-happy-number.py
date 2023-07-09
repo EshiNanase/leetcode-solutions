4a2763fcf2d4978d99865c0b6c1957a80e23fd63
Aleksandr Piksasov <alksndr.zln@gmail.com>
2023-07-09 23:10:12 +0300

    Time: 56 ms (25.62%), Space: 16.1 MB (95.20%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -1,11 +1,15 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        answer = n
-        for i in range(10):
-            numbers = [int(i) for i in str(answer)]
-            answer = 0
-            for i in numbers:
-                answer += i**2
-        if answer == 1:
-            return True
-        return False
+        sys.set_int_max_str_digits(9999999)
+        count = 0
+        while n != 1 and count < 30:
+            digits = list(str(n))
+            for digit_ind, digit in enumerate(digits):
+                digits[digit_ind] = int(digit)**2
+            n = sum(digits)
+            count += 1
+        
+        if count == 30:
+            return False
+        
+        return True
