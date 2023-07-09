084d806d9ff67c91f0092aed609d83ff831f2cf2
Aleksandr Piksasov <alksndr.zln@gmail.com>
2023-07-09 17:29:00 +0300

    Time: 69 ms (90.76%), Space: 17.7 MB (35.55%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -2,8 +2,9 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashmap = {}
         for num_ind, num in enumerate(nums):
-            for existing_num in hashmap:
-                if existing_num + num == target:
-                    return sorted([hashmap[existing_num], num_ind])
+            difference = target - num
+            needed_number = hashmap.get(difference)
+            if needed_number is not None:
+                return sorted([needed_number, num_ind])
             hashmap[num] = num_ind
         return []
